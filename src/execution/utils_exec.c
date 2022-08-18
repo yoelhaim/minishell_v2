55c6de4d928576b6627ee210d83367a70deb3d6b
yoelhaim <youssefelhaimer8@gmail.com>
2022-08-18 23:38:05 +0100

    add  functions in utils :tada:

---
+++ src/execution/utils_exec.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils_exec.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yoelhaim <yoelhaim@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/18 22:34:27 by yoelhaim          #+#    #+#             */
+/*   Updated: 2022/08/18 23:02:34 by yoelhaim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../minishell.h"
+char **export_env(t_env *env)
+{
+	t_env *tmp;
+	int len;
+	char **exported_env;
+	int i;
+
+	i = 0;
+	len = 0;
+	tmp = env;
+	exported_env = NULL;
+	while (tmp && ++len)
+		tmp = tmp->next;
+	exported_env = malloc(sizeof(char *) * len++);
+	add(&g_tools.garbage, export_env);
+	while (env)
+	{
+		exported_env[i++] = env->value;
+		env = env->next;
+	}
+	exported_env[i] = NULL;
+	return (exported_env);
+}
+
+char *get_path(void)
+{
+	t_env *env;
+	char *splited_path;
+
+	env = g_tools.g_env;
+
+	while (env)
+	{
+		if (strstr(env->variable, "PATH"))
+		{
+			splited_path = strstr(env->value, "=") + 1;
+			return (splited_path);
+		}
+		env = env->next;
+	}
+
+	return (NULL);
+}
