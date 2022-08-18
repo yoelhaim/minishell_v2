b6ff84b69c14f095e8fa5dad983f1e9961b7b4c9
yoelhaim <youssefelhaimer8@gmail.com>
2022-08-18 23:37:18 +0100

    norm main  :fire:

--- src/main.c
+++ src/main.c
@@ -3,39 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: akadi <akadi@student.42.fr>                +#+  +:+       +#+        */
+/*   By: yoelhaim <yoelhaim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/19 20:48:33 by yoelhaim          #+#    #+#             */
-/*   Updated: 2022/08/18 20:21:13 by akadi            ###   ########.fr       */
+/*   Updated: 2022/08/18 23:28:44 by yoelhaim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-t_globals g_tools = {0};
+t_globals	g_tools = {0};
 
-int main(int ac, char **av, char **envr)
+void	setup_shell(t_node **datas, t_cmd **cmds)
 {
-	t_cmd *cmd;
-	char *line;
-	t_node *data;
-	(void)av;
-	(void)envr;
-	if (ac != 1)
-		return (printf("ERROR"), 0);
-	create_env(envr);
+	char	*line;
+	t_cmd	*cmd;
+	t_node	*data;
+
+	cmd = *cmds;
+	data = *datas;
 	while (1)
 	{
 		line = read_line();
 		if (!line || !is_ws(line))
 		{
 			free(line);
-			continue;
+			continue ;
 		}
 		if (check_lexer(&data, line) == ERROR_RETURN)
 		{
 			clear_list(&data);
-			continue;
+			continue ;
 		}
 		if (syntax_error(data) == ERROR_RETURN)
 			clear_list(&data);
@@ -43,7 +41,19 @@ int main(int ac, char **av, char **envr)
 		exec_cmd(cmd);
 		clear_list(&data);
 	}
-	free_all(g_tools.garbage);
+}
+
+int	main(int ac, char **av, char **envr)
+{
+	t_cmd	*cmd;
+	t_node	*data;
+
+	(void)av;
+	if (ac != 1)
+		return (printf("ERROR"), 0);
+	create_env(envr);
+	setup_shell (&data, &cmd);
+	free_all (g_tools.garbage);
 	rl_clear_history();
 	return (0);
 }
