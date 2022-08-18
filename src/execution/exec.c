992934336e68e20afb29b306081e55e37a59cbc8
yoelhaim <youssefelhaimer8@gmail.com>
2022-08-18 23:37:47 +0100

    exut functions :tada:

--- src/execution/exec.c
+++ src/execution/exec.c
@@ -3,61 +3,63 @@
 /*                                                        :::      ::::::::   */
 /*   exec.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: akadi <akadi@student.42.fr>                +#+  +:+       +#+        */
+/*   By: yoelhaim <yoelhaim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/18 20:12:27 by akadi             #+#    #+#             */
-/*   Updated: 2022/08/18 20:56:00 by akadi            ###   ########.fr       */
+/*   Updated: 2022/08/18 23:04:03 by yoelhaim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
 
-int	check_builtin(char	*cmd)
+int check_builtin(char *cmd)
 {
-	if (!ft_strcmp(cmd, "echo")|| !ft_strcmp(cmd, "cd")\
-	|| !ft_strcmp(cmd, "export")||!ft_strcmp(cmd, "unset")\
-	||!ft_strcmp(cmd, "exit")||!ft_strcmp(cmd, "pwd")\
-	||!ft_strcmp(cmd, "env"))
-		return(1);
+	if (!ft_strcmp(cmd, "echo") || !ft_strcmp(cmd, "cd") || !ft_strcmp(cmd, "export") || !ft_strcmp(cmd, "unset") || !ft_strcmp(cmd, "exit") || !ft_strcmp(cmd, "pwd") || !ft_strcmp(cmd, "env"))
+		return (1);
 	return (0);
-} 
-
-void print_cmnd(char  **cmd)
-{
-	char *tmp;
-	tmp = *cmd;
-	
-	printf("zawa9a => %s\n", cmd[0]);
-	execve("/bin/ls", cmd, &g_tools.g_env->value);
 }
 
-void cmd_system(char  **cmd)
+void print_cmnd(char **cmd)
+{
+	char *tmp;
+	char **splited_path;
+
+	tmp = *cmd;
+	splited_path = ft_split(get_path(), ':');
+	// printf(" path is => %s\n", get_path());
+	while (*splited_path)
+	{
+		execve(ft_strjoin(ft_strjoin("/", *splited_path), ft_strjoin("/", cmd[0])), cmd, export_env(g_tools.g_env));
+		splited_path++;
+	}
+}
+
+void cmd_system(char **cmd)
 {
 	int pid;
+	(void)cmd;
 	pid = fork();
-	if(pid == -1)
-		return ;
 	if (pid == 0)
 	{
-		 print_cmnd(cmd);
-		exit(EXIT_SUCCESS);	
+		print_cmnd(cmd);
+		exit(1);
 	}
 	else
 		pid = wait(NULL);
 }
-void	exec_cmd(t_cmd *cmd)
+void exec_cmd(t_cmd *cmd)
 {
-	t_cmd	*tmp;
-	
+	t_cmd *tmp;
+
 	tmp = cmd;
 	while (tmp)
 	{
-		if(*(tmp->cmnd) == NULL)
+		if (*(tmp->cmnd) == NULL)
 		{
-			return ;
+			return;
 		}
 		if (check_builtin(*(tmp->cmnd)))
-			printf("%s\n", *(tmp->cmnd));
+			printf(" build => %s\n", *(tmp->cmnd));
 		else
 			cmd_system(tmp->cmnd);
 		tmp = tmp->next;
