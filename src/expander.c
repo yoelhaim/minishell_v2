437ee671cfba638fca87603328a71fd36b40c8af
yoelhaim <youssefelhaimer8@gmail.com>
2022-08-20 12:09:11 +0100

    add status exit :tada:

--- src/expander.c
+++ src/expander.c
@@ -6,12 +6,15 @@
 /*   By: yoelhaim <yoelhaim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/20 09:19:50 by yoelhaim          #+#    #+#             */
-/*   Updated: 2022/08/20 11:33:05 by yoelhaim         ###   ########.fr       */
+/*   Updated: 2022/08/20 12:07:56 by yoelhaim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
-
+void	check_expand_status(char **value)
+{
+	*value = ft_itoa(g_tools.status_sign);
+}
 void	check_expand_dollar(char **value)
 {
 	char	*val;
@@ -19,24 +22,30 @@ void	check_expand_dollar(char **value)
 
 	val = *value;
 	env = g_tools.g_env;
-	if (val[0] == '0' && val[1] == '\0')
-		*value = "minishell";
-	if (val[0] >= '1' && val[0] <= '9' && val[1] == '\0')
-		*value = "\0";
-	if (val[0] >= '1' && val[0] <= '9' && val[1] != '\0')
-		*value = "\0";
-	else
+
+
+	while (env)
 	{
-		while (env)
+		if (!ft_strcmp(env->variable, val))
 		{
-			if (!ft_strcmp(env->variable, val))
-			{
-				*value = strstr(env->value, "=") + 1;
-				break ;
-			}
-			env = env->next;
+			*value = strstr(env->value, "=") + 1;
+			break ;
 		}
+		else
+		{
+			if (val[0] == '0' && val[1] == '\0')
+				*value = "minishell";
+			else if (val[0] >= '1' && val[0] <= '9' && val[1] == '\0')
+				*value = "\0";
+			else if (val[0] >= '1' && val[0] <= '9' && val[1] != '\0')
+				*value = "\0";
+			else
+				*value = "\0";
+		}
+
+		env = env->next;
 	}
+
 }
 
 void	expander(t_node **list)
@@ -46,8 +55,10 @@ void	expander(t_node **list)
 	cmd = *list;
 	while (cmd)
 	{
-		if (cmd->type == 9)
+		if (cmd->type == SIGN)
 			check_expand_dollar(&cmd->val);
+		if (cmd->type == EXIT_STATUS)
+			check_expand_status(&cmd->val);
 		cmd = cmd->next;
 	}
 }
