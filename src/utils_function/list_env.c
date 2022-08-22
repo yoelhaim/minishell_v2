cfc71c12b298f280c8e34bf6a40747d26846fb07
yoelhaim <youssefelhaimer8@gmail.com>
2022-08-22 23:08:34 +0100

    add size the node env :tada:

--- src/utils_function/list_env.c
+++ src/utils_function/list_env.c
@@ -6,7 +6,7 @@
 /*   By: yoelhaim <yoelhaim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/16 19:00:14 by yoelhaim          #+#    #+#             */
-/*   Updated: 2022/08/21 23:24:07 by yoelhaim         ###   ########.fr       */
+/*   Updated: 2022/08/22 22:08:29 by yoelhaim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,6 +41,21 @@ void	pushback_env(t_env **lst, char *variable, char *val)
 			head = head->next;
 		head->next = new;
 	}
+}
+
+int size_of_env(void)
+{
+	int	i;
+	t_env *env;
+	
+	env = g_tools.g_env;
+	i = 0;
+	while(env)
+	{
+		i++;
+		env = env->next;
+	}
+	return(i);
 }
 
 t_env	*clear_list_env(t_env **lst)
