819f3830159c865888934d622b66427b37eaa2c5
yoelhaim <youssefelhaimer8@gmail.com>
2022-08-27 23:06:49 +0100

    norminette  exec :rocket:

--- src/execution/exec.c
+++ src/execution/exec.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exec.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: akadi <akadi@student.42.fr>                +#+  +:+       +#+        */
+/*   By: yoelhaim <yoelhaim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/25 12:28:53 by yoelhaim          #+#    #+#             */
-/*   Updated: 2022/08/27 19:52:12 by akadi            ###   ########.fr       */
+/*   Updated: 2022/08/27 22:30:07 by yoelhaim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,9 @@
 int	check_builtin(char *cmd)
 {
 	if (!ft_strcmp(cmd, "echo") \
-	|| !ft_strcmp(cmd, "cd") || !ft_strcmp(cmd, "export") \
-	|| !ft_strcmp(cmd, "unset") || !ft_strcmp(cmd, "exit") \
-	|| !ft_strcmp(cmd, "pwd") || !ft_strcmp(cmd, "env"))
+			|| !ft_strcmp(cmd, "cd") || !ft_strcmp(cmd, "export") \
+			|| !ft_strcmp(cmd, "unset") || !ft_strcmp(cmd, "exit") \
+			|| !ft_strcmp(cmd, "pwd") || !ft_strcmp(cmd, "env"))
 		return (1);
 	return (0);
 }
@@ -26,7 +26,7 @@ int	print_cmnd(char **cmd)
 {
 	char	**splited_path;
 	char	*path;
-	
+
 	(void ) cmd;
 	splited_path = ft_split(get_path(), ':');
 	if (!access(*cmd, X_OK))
@@ -35,187 +35,66 @@ int	print_cmnd(char **cmd)
 	{	
 		while (*splited_path)
 		{
-			path = ft_strjoin(*splited_path,ft_strjoin("/", cmd[0]));
-			if(!access(path, X_OK))
-				break;
+			path = ft_strjoin(*splited_path, ft_strjoin("/", cmd[0]));
+			if (!access(path, X_OK))
+				break ;
 			free(path);
 			free(*splited_path);
-		splited_path++;
-	}
+			splited_path++;
+		}
 	}
 	execve(path, cmd, export_env(g_tools.g_env));
-	printf("minishell : %s : command not found \n", *cmd );
-	return(ERROR_RETURN);	
+	printf("minishell : %s : command not found \n", *cmd);
+	return (ERROR_RETURN);
 }
 
-int cmd_systm_one(t_cmd *cmd)
+void	is_red(t_red *cmd, int *status)
 {
-	int pid;
-	int	statuss;
-	int out;
-	out = dup(1);
-	int in = dup(0);
-	int status = 1;
-	
-	(void)in;
-	pid = fork();
-	if(pid == 0)
-	{
-		if(check_redirecrt(cmd->red,&status) == ERROR_RETURN)
-			return(1);
-		print_cmnd(cmd->cmnd);
-		exit(127);
-	}
-	waitpid(-1, &statuss, 0);
-	if(status)
-		{
-		  dup2(out , 1);
-		  close(out);
-		}
-		else
-		{
-		 dup2(in , 0);
-		 close(in);
-		 dup2(out , 1);
-		 close(out);
-		}	
-	g_tools.status_sign = WEXITSTATUS(statuss);
-	return (1);
-}
+	t_red	*red;
 
-int 	check_is_one_cmnd(t_cmd *cmd, t_node *list)
-{
-	int	i;
-	int out;
-	out = dup(1);
-	int in = dup(0);
-	int status = 1;
-	i = 0;
-	while (list)
-	{
-		if(list->type ==  PIPE)
-			i++;
-		list = list->next;
-	}
-	if(i == 0)
-	{	
-		if(check_redirecrt(cmd->red, &status) == ERROR_RETURN)
-			return(1);
-		if(*cmd->cmnd != NULL)
-		{
-		if(check_builtin(*cmd->cmnd))
-			ft_builtin(cmd->cmnd);
-		else
-			cmd_systm_one(cmd);
-		}
-		if(status)
-		{
-		  dup2(out , 1);
-		  close(out);
-		}
-		else
-		{
-		 dup2(in , 0);
-		  close(in);
-		  dup2(out , 1);
-		  close(out);
-		}
-		return(1);
-	}
-	return(0);
-}
-
-int is_red(t_red *cmd, int *status)
-{
-	t_red *red;
 	red = cmd;
-	(void) status;
 	while (red)
 	{
-		if(check_redirecrt(cmd, status) != ERROR_RETURN)
-			return 1;
+		if (check_redirecrt(cmd, status) != ERROR_RETURN)
+			return ;
 		red = red->next;
 	}
-	return (0);
 }
 
-void	pipe_cmd(t_cmd *cmd, t_node *list)
+void	pipe_cmd(t_cmd *cmd)
 {
-	(void)list;
-	int fd[2];
-	// int out = dup(1);
-	// int in = dup(0);
-	pid_t pid;
-	int status = 1;		
-	while (cmd) 
-	{
+	pid_t	pid;
+	int		status;
 
-		pipe(fd);
-		if ((pid = fork()) == -1) {
-			perror("fork");
-			exit(1);
-		}
-		else if (pid == 0) 
-		{
-			close(fd[0]);
-			if (g_tools.fdd != 0)
-			{
-				dup2(g_tools.fdd, 0);
-				close(g_tools.fdd);
-			}
-			if (cmd->next != NULL) {
-				close(fd[0]);
-				dup2(fd[1], 1);
-			}
-			close(fd[1]);
-			if (is_red(cmd->red, &status))
-			{
-				/*if (!status)
-				{
-					dup2(out, 1);
-					close(out);
-				}
-				else
-				{
-					dup2(in, 1);
-					close(in);
-				}*/
-				printf("--------\n");
-			}
-			if (check_builtin(*(cmd->cmnd)) )
-			{
-				if (g_tools.status_sign == 127)
-			 		g_tools.status_sign = 1;
-				else
-					g_tools.status_sign = 0;
-				ft_builtin(cmd->cmnd);
-				exit(1);
-			}
-			else 
-			{
-					print_cmnd(cmd->cmnd);
-					//exit(1);
-			}
-		}
-			close(fd[1]);
-			if (g_tools.fdd != 0)
-				close(g_tools.fdd);
-			if (!cmd->next)
-				close(fd[0]);
-			g_tools.fdd = fd[0];
-			cmd = cmd->next;	
+	status = 1;
+	while (cmd)
+	{
+		pipe(g_tools.fd);
+		pid = fork();
+		if (pid == 0)
+			child_process(cmd, &status);
+		close(g_tools.fd[1]);
+		if (g_tools.fdd != 0)
+			close(g_tools.fdd);
+		if (!cmd->next)
+			close(g_tools.fd[0]);
+		g_tools.fdd = g_tools.fd[0];
+		cmd = cmd->next;
 	}
-	while(1)
-		{
-			if (wait(NULL) == -1)
-				break;
-		}
-	
+	while (1)
+	{
+		if (wait(NULL) == -1)
+			break ;
+	}
 }
+
 void	exec_cmd(t_cmd *cmd, t_node *list)
 {
-	if(check_is_one_cmnd(cmd, list))
+	int	i;
+
+	i = 0;
+	if (check_is_one_cmnd(cmd, list, &i))
 		return ;
 	else
-		pipe_cmd(cmd, list);
+		pipe_cmd(cmd);
 }
---
+++ src/execution/exec_2.c
@@ -0,0 +1,111 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exec_2.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yoelhaim <yoelhaim@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/27 20:24:16 by yoelhaim          #+#    #+#             */
+/*   Updated: 2022/08/27 22:58:10 by yoelhaim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../minishell.h"
+
+void	check_status_file(int status)
+{
+	// printf("stt %d\n", status);
+	if (status)
+	{
+		dup2(g_tools.dup_out, 1);
+		close(g_tools.dup_out);
+	}
+	else
+	{
+		dup2(g_tools.dup_in, 0);
+		close(g_tools.dup_in);
+		dup2(g_tools.dup_out, 1);
+		close(g_tools.dup_out);
+	}	
+}
+
+int	cmd_systm_one(t_cmd *cmd)
+{
+	int	pid;
+	int	status;
+	int	statuss;
+
+	g_tools.dup_out = dup(1);
+	g_tools.dup_in = dup(0);
+	pid = fork();
+	status = 1;
+	if (pid == 0)
+	{
+		if (check_redirecrt(cmd->red, &status) == ERROR_RETURN)
+			return (1);
+		print_cmnd(cmd->cmnd);
+		exit(0);
+	}
+	waitpid (-1, &statuss, 0);
+	check_status_file(status);
+	g_tools.status_sign = WEXITSTATUS(statuss);
+	return (1);
+}
+
+int	check_is_one_cmnd(t_cmd *cmd, t_node *list, int *i)
+{
+	int		status;
+
+	g_tools.dup_out = dup(1);
+	g_tools.dup_in = dup(0);
+	status = 1;
+	while (list)
+	{
+		if (list->type == PIPE)
+			i++;
+		list = list->next;
+	}
+	if (*i == 0)
+	{	
+		if (check_redirecrt(cmd->red, &status) == ERROR_RETURN)
+			return (1);
+		if (*cmd->cmnd != NULL)
+		{
+			if (check_builtin(*cmd->cmnd))
+				ft_builtin(cmd->cmnd);
+			else
+				cmd_systm_one(cmd);
+		}
+		check_status_file(status);
+		return (1);
+	}
+	return (0);
+}
+
+void	child_process(t_cmd *cmd, int *status)
+{
+	close(g_tools.fd[0]);
+	if (g_tools.fdd != 0)
+	{
+		dup2(g_tools.fdd, 0);
+		close(g_tools.fdd);
+	}
+	if (cmd->next != NULL)
+	{
+		close(g_tools.fd[0]);
+		dup2(g_tools.fd[1], 1);
+	}
+	close(g_tools.fd[1]);
+	is_red(cmd->red, status);
+	if (check_builtin(*(cmd->cmnd)))
+	{
+		if (g_tools.status_sign == 127)
+			g_tools.status_sign = 1;
+		else
+			g_tools.status_sign = 0;
+		ft_builtin(cmd->cmnd);
+		exit(1);
+	}
+	else
+		print_cmnd(cmd->cmnd);
+}
