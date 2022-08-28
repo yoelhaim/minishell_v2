7e56396a0f7f02ca3a86ecf0d9fe61cfb25851ab
yoelhaim <youssefelhaimer8@gmail.com>
2022-08-28 23:55:05 +0100

    norminetee :rocket:

--- src/execution/red_cheker.c
+++ src/execution/red_cheker.c
@@ -6,7 +6,7 @@
 /*   By: yoelhaim <yoelhaim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/26 16:00:23 by yoelhaim          #+#    #+#             */
-/*   Updated: 2022/08/27 23:37:48 by yoelhaim         ###   ########.fr       */
+/*   Updated: 2022/08/28 22:46:31 by yoelhaim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,9 +31,32 @@ int	open_in(char *filename, int *status)
 	g_tools.r_in = open(filename, O_RDONLY, 0644);
 	if (g_tools.r_in == -1)
 	{
+		*status = 0;
+		dup2(g_tools.r_in, 0);
+		close(g_tools.r_in);
 		ft_putstr_fd("No such file or directory\n", 2);
 		return (ERROR_RETURN);
 	}
+	*status = 0;
+	dup2(g_tools.r_in, 0);
+	close(g_tools.r_in);
+	return (1);
+}
+
+int	open_herdoc_file(int *status, t_red *cmd)
+{
+	static int	len;
+
+	(void)cmd;
+	len++;
+	g_tools.r_in = open(".herdoc", O_RDONLY, 0644);
+	if (g_tools.r_in == -1)
+	{
+		ft_putstr_fd("No such file or directory\n", 2);
+		return (ERROR_RETURN);
+	}
+	if (len == 0)
+		unlink(".herdoc");
 	*status = 0;
 	dup2(g_tools.r_in, 0);
 	close(g_tools.r_in);
