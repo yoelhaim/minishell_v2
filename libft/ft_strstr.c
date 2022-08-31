802eed9d672a0be048b38fc38863b54e7e8a5dd5
yoelhaim <youssefelhaimer8@gmail.com>
2022-08-31 23:21:42 +0100

    new function strstr :rocket:

---
+++ libft/ft_strstr.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yoelhaim <yoelhaim@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/30 21:07:25 by yoelhaim          #+#    #+#             */
+/*   Updated: 2022/08/30 21:07:47 by yoelhaim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strstr(char *str, char *to_find)
+{
+	int	i;
+	int	j;
+	int	k;
+
+	i = 0;
+	j = 0;
+	k = 0;
+	while (to_find[k])
+		k++;
+	if (k == 0)
+		return (str);
+	while (str[i])
+	{
+		while (to_find[j] == str[i + j])
+		{
+			if (j == k - 1)
+				return (str + i);
+			j++;
+		}
+		j = 0;
+		i++;
+	}
+	return (0);
+}
