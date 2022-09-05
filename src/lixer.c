1e834b82081293b696095df934cd26f2cb01617a
yoelhaim <youssefelhaimer8@gmail.com>
2022-09-05 16:42:59 +0100

    rm \

--- src/lixer.c
+++ src/lixer.c
@@ -6,7 +6,7 @@
 /*   By: yoelhaim <yoelhaim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/01 16:54:42 by yoelhaim          #+#    #+#             */
-/*   Updated: 2022/09/05 16:28:25 by yoelhaim         ###   ########.fr       */
+/*   Updated: 2022/09/05 16:42:44 by yoelhaim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ char	*check_is_wd(t_node **list, char *line, char *sc)
 	if (i != 0)
 	{
 		buff[i] = 0;
-		if (ft_strlen(sc) == 13 || ft_strlen(sc) == 1)
+		if (ft_strlen(sc) == 12 || ft_strlen(sc) == 1)
 			pushback(list, WORD, buff);
 		else
 			pushback(list, SIGN, buff);
@@ -119,7 +119,7 @@ int	check_lexer(t_node **list, char *line)
 	while (*line)
 	{
 		line = check_is_ws(list, line);
-		line = check_is_wd(list, line, " \t\r\n\"'\v\f\\|<>$");
+		line = check_is_wd(list, line, " \t\r\n\"'\v\f|<>$");
 		line = check_is_pipe(list, line);
 		line = check_is_red(list, line);
 		line = check_is_sign(list, line);
